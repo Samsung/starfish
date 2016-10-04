0d0f5db87945c6e3e82f0fa8c9bfd586db6da4d5
Jiyun Yang <ji.yang@samsung.com>
2016-10-04 20:07:12 +0900

    Remove unnecessary header include

--- src/extra/SourceBuffer.cpp
+++ src/extra/SourceBuffer.cpp
@@ -21,7 +21,6 @@
 #include "dom/Event.h"
 #include "dom/DOMException.h"
 #include "dom/HTMLMediaElement.h"
-#include "platform/multimedia/MediaSourceClient.h"
 #include "platform/multimedia/MediaPlayer.h"
 #include "platform/message_loop/MessageLoop.h"
 #include "MediaSource.h"
