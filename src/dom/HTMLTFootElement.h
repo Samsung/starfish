a879cfb7f250fbc768105d1eb5764839bf66eb66
Ryan Choi <ryan.choi@samsung.com>
2016-12-14 15:20:51 +0900

    Add missing Table-related *.h files

---
+++ src/dom/HTMLTFootElement.h
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#ifndef __StarFishHTMLTFootElement__
+#define __StarFishHTMLTFootElement__
+
+#include "dom/HTMLElement.h"
+
+namespace StarFish {
+
+class HTMLTFootElement : public HTMLElement {
+public:
+    HTMLTFootElement(Document* document)
+        : HTMLElement(document)
+    {
+    }
+
+    virtual void initScriptObject(ScriptBindingInstance* instance)
+    {
+        initScriptWrappable(this);
+    }
+
+    /* 4.4 Interface Node */
+
+    virtual String* localName()
+    {
+        return document()->window()->starFish()->staticStrings()->m_tfootTagName.localName();
+    }
+
+    virtual QualifiedName name()
+    {
+        return document()->window()->starFish()->staticStrings()->m_tfootTagName;
+    }
+
+    /* Other methods (not in DOM API) */
+
+    virtual bool isHTMLTFootElement() const
+    {
+        return true;
+    }
+
+protected:
+};
+
+}
+
+#endif
