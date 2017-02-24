5345aa84045963333e0633d19dd27061e7e330b1
Youngil Choi <duddlf.choi@samsung.com>
2017-02-24 16:45:35 +0900

    Apply new coding rule /inc
    Signed-off-by: Youngil Choi <duddlf.choi@samsung.com>

--- inc/StarFishPublic.h
+++ inc/StarFishPublic.h
@@ -27,20 +27,24 @@ typedef struct StarFishInstance {
     void* m_starfish;
 } StarFishInstance;
 
-STARFISH_EXPORT StarFishInstance* starfishCreate(void* window, int windowWidth, int windowHeight, const char* locale, const char* timezoneID, float defaultFontSizeMultiplier);
+STARFISH_EXPORT StarFishInstance* starfishCreate(
+    void* window, int windowWidth, int windowHeight, const char* locale,
+    const char* timezoneID, float defaultFontSizeMultiplier);
 STARFISH_EXPORT void starfishRemove(StarFishInstance* instance);
 
-STARFISH_EXPORT void starfishLoadHTMLDocument(StarFishInstance* instance, const char* path);
+STARFISH_EXPORT void starfishLoadHTMLDocument(StarFishInstance* instance,
+                                              const char* path);
 
 STARFISH_EXPORT void starfishNotifyPause(StarFishInstance* instance);
 STARFISH_EXPORT void starfishNotifyResume(StarFishInstance* instance);
 
-STARFISH_EXPORT void registerFileOpenCB(FILE * (*cb) (const char* fileName));
-STARFISH_EXPORT void registerFileLengthCB(long int (*cb) (FILE* stream));
-STARFISH_EXPORT void registerFileReadCB(size_t (*cb) (void * buf, size_t size, size_t count, FILE * fp));
-STARFISH_EXPORT void registerFileCloseCB(int (*cb) (FILE * fp));
-STARFISH_EXPORT void registerFileMatchLocationCB(const char* (*cb) (const char* fileName));
-
+STARFISH_EXPORT void registerFileOpenCB(FILE* (*cb)(const char* fileName));
+STARFISH_EXPORT void registerFileLengthCB(long int (*cb)(FILE* stream));
+STARFISH_EXPORT void registerFileReadCB(size_t (*cb)(void* buf, size_t size,
+                                                     size_t count, FILE* fp));
+STARFISH_EXPORT void registerFileCloseCB(int (*cb)(FILE* fp));
+STARFISH_EXPORT void registerFileMatchLocationCB(
+    const char* (*cb)(const char* fileName));
 
 #ifdef __cplusplus
 }
