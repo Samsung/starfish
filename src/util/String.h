f8bb8eaf6202e7d02961ea42d735e629f9c3a1d0
Youngil Choi <duddlf.choi@samsung.com>
2017-03-06 19:04:24 +0900

    Apply new coding style rule for minor fix
    Signed-off-by: Youngil Choi <duddlf.choi@samsung.com>

--- src/util/String.h
+++ src/util/String.h
@@ -341,9 +341,10 @@ public:
         if (CHAR >= 0x7F && CHAR < 0xA0) {
             return true;
         }
-        if (CHAR == 0xAD || CHAR == 0x200B || CHAR == 0x200E || CHAR == 0x200F ||
-            CHAR == 0x202A || CHAR == 0x202B || CHAR == 0x202C || CHAR == 0x202D ||
-            CHAR == 0x202C || CHAR == 0x202E || CHAR == 0xFEFF || CHAR == 0xFFFC) {
+        if (CHAR == 0xAD || CHAR == 0x200B || CHAR == 0x200E ||
+            CHAR == 0x200F || CHAR == 0x202A || CHAR == 0x202B ||
+            CHAR == 0x202C || CHAR == 0x202D || CHAR == 0x202C ||
+            CHAR == 0x202E || CHAR == 0xFEFF || CHAR == 0xFFFC) {
             return true;
         }
         return false;
