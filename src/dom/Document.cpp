993f7fce9c479af38e65e13cb9b2611d6392e3a8
Ryan Choi <ryan.choi@samsung.com>
2017-03-13 11:25:04 +0900

    Add td { padding: 1px } to the user agent style sheet
    Signed-off-by: Ryan Choi <ryan.choi@samsung.com>

--- src/dom/Document.cpp
+++ src/dom/Document.cpp
@@ -425,6 +425,26 @@ Document::Document(Window* window, ScriptBindingInstance* scriptBindingInstance,
         pair.setValue(data);
         rule->styleDeclaration()->addValuePair(pair);
 
+        pair.setKeyKind(CSSStyleValuePair::PaddingTop);
+        pair.setValueKind(CSSStyleValuePair::Length);
+        pair.setLengthValue("1px");
+        rule->styleDeclaration()->addValuePair(pair);
+
+        pair.setKeyKind(CSSStyleValuePair::PaddingRight);
+        pair.setValueKind(CSSStyleValuePair::Length);
+        pair.setLengthValue("1px");
+        rule->styleDeclaration()->addValuePair(pair);
+
+        pair.setKeyKind(CSSStyleValuePair::PaddingBottom);
+        pair.setValueKind(CSSStyleValuePair::Length);
+        pair.setLengthValue("1px");
+        rule->styleDeclaration()->addValuePair(pair);
+
+        pair.setKeyKind(CSSStyleValuePair::PaddingLeft);
+        pair.setValueKind(CSSStyleValuePair::Length);
+        pair.setLengthValue("1px");
+        rule->styleDeclaration()->addValuePair(pair);
+
         userAgentStyleSheet->addRule(rule);
     }
 
