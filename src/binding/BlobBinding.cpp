e4b9476ba1139dfc89aca43d95936c525f0f6a84
Jiyun Yang <ji.yang@samsung.com>
2017-04-10 18:21:51 +0900

    Separate constuctor code from BlobBinding.cpp to prepare applying binding generator
    Signed-off-by: Jiyun Yang <ji.yang@samsung.com>

--- src/binding/BlobBinding.cpp
+++ src/binding/BlobBinding.cpp
@@ -20,108 +20,11 @@
 #include "Binding.h"
 #include "binding/escargot/ScriptBindingInstanceDataEscargot.h"
 
+#include "BlobCustomBinding.h"
+
 namespace StarFish {
 
 using namespace escargot;
-
-static ESValue blobFunction(ESVMInstance* instance)
-{
-    // https://www.w3.org/TR/FileAPI/#blob-constructor-steps
-    int argCount = instance->currentExecutionContext()->argumentCount();
-    if (argCount == 0) {
-        Window* w = ((Window*)ESVMInstance::currentInstance()
-                         ->globalObject()
-                         ->extraPointerData());
-        Blob* b = new Blob(w->starFish(), 0, String::emptyString, nullptr,
-                           false, false);
-        return b->scriptValue();
-    }
-    ESValue firstArg = instance->currentExecutionContext()->readArgument(0);
-
-    void* bytes = 0;
-    ESValue lengthString = currentInstance->strings().length.string();
-    ESObject* obj = nullptr;
-    ESValue lengthValue;
-
-    if (!firstArg.isObject() ||
-        (lengthValue = (obj = firstArg.toObject())->get(lengthString))
-            .isUndefinedOrNull()) {
-        auto msg = ESString::create(
-            "Failed to construct 'Blob': The 1st argument is "
-            "neither "
-            "an array, nor does it have indexed properties.");
-        instance->throwError(ESValue(TypeError::create(msg)));
-    }
-
-    size_t length = (size_t)lengthValue.toNumber();
-
-    GCVector<std::pair<void*, size_t>> bufferInfo;
-    size_t totalByteLength = 0;
-    for (size_t i = 0; i < length; i++) {
-        ESValue element = obj->get(ESValue(i));
-
-#ifdef USE_ES6_FEATURE
-        // ESArrayBufferView
-        if (element.isESPointer() &&
-            element.asESPointer()->isESArrayBufferView()) {
-            ESArrayBufferView* v = element.asESPointer()->asESArrayBufferView();
-            const char* p = (const char*)v->buffer()->data();
-            p += v->byteoffset();
-            bufferInfo.push_back(std::make_pair((void*)p, v->bytelength()));
-            totalByteLength += v->bytelength();
-        }
-
-        // ESArrayBufferObject
-        if (element.isESPointer() &&
-            element.asESPointer()->isESArrayBufferObject()) {
-            ESArrayBufferObject* v =
-                element.asESPointer()->asESArrayBufferObject();
-            bufferInfo.push_back(
-                std::make_pair((void*)v->data(), v->bytelength()));
-            totalByteLength += v->bytelength();
-        }
-#endif
-        // Blob
-        if (element.isObject()) {
-            ESObject* o = element.toObject();
-            if (o->extraData() == kEscargotObjectCheckMagic &&
-                ((ScriptWrappable*)o->extraPointerData())->isBlob()) {
-                Blob* bb = (Blob*)o->extraPointerData();
-                bufferInfo.push_back(std::make_pair(bb->data(), bb->size()));
-                totalByteLength += bb->size();
-                continue;
-            }
-        }
-
-        // otherwise, toString()
-        NullableUTF8String s =
-            toBrowserString(element.toString())->toNullableUTF8String();
-        bufferInfo.push_back(std::make_pair((void*)s.m_buffer, s.m_bufferSize));
-        totalByteLength += s.m_bufferSize;
-    }
-
-    size_t offset = 0;
-    char* buffer = (char*)GC_MALLOC_ATOMIC_IGNORE_OFF_PAGE(totalByteLength);
-    for (size_t i = 0; i < bufferInfo.size(); i++) {
-        memcpy(buffer + offset, bufferInfo[i].first, bufferInfo[i].second);
-        offset += bufferInfo[i].second;
-    }
-
-    STARFISH_ASSERT(offset == totalByteLength);
-
-    ESValue secondArg = instance->currentExecutionContext()->readArgument(1);
-    String* type = String::emptyString;
-    if (!secondArg.isUndefinedOrNull()) {
-        type = toBrowserString(secondArg.toString())->toLower();
-    }
-
-    Window* w = ((Window*)ESVMInstance::currentInstance()
-                     ->globalObject()
-                     ->extraPointerData());
-    Blob* newBlob =
-        new Blob(w->starFish(), totalByteLength, type, buffer, false, false);
-    return newBlob->scriptValue();
-}
 
 static ESValue sizeGetterFunction(ESVMInstance* instance)
 {
@@ -163,7 +66,7 @@ ESFunctionObject* bindingBlob(ScriptBindingInstance* scriptBindingInstance)
 {
     /* Blob */
     auto fnBlob = ESFunctionObject::create(
-        NULL, blobFunction, ESString::create("Blob"), 0, true, true);
+        NULL, blobCustomFunction, ESString::create("Blob"), 0, true, true);
     fnBlob->defineAccessorProperty(
         ESVMInstance::currentInstance()->strings().prototype.string(),
         ESVMInstance::currentInstance()->functionPrototypeAccessorData(), false,
