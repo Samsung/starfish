f7ed551b0ffeb95d88412205b6bbefb07029e628
Ryan Choi <ryan.choi@samsung.com>
2017-06-01 15:06:46 +0900

    Add local storage interfaces
    * This commit prepares Storage architecture.
    
    Signed-off-by: Ryan Choi <ryan.choi@samsung.com>

---
+++ src/core/page/SecurityOriginData.h
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2017-present Samsung Electronics Co., Ltd
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#ifndef __StarFishSecurityOriginData__
+#define __StarFishSecurityOriginData__
+
+#include "StarFishConfig.h"
+
+namespace StarFish {
+
+class SecurityOriginData : public gc {
+public:
+    SecurityOriginData(String* protocol, String* host, unsigned port)
+        : m_protocol(protocol)
+        , m_host(host)
+        , m_port(port)
+    {
+    }
+
+    String* protocol() const
+    {
+        return m_protocol;
+    }
+
+    String* host() const
+    {
+        return m_host;
+    }
+
+    unsigned port() const
+    {
+        return m_port;
+    }
+
+    bool operator==(const SecurityOriginData& other) const
+    {
+        return protocol()->equals(other.protocol()) &&
+               host()->equals(other.host()) && port() == other.port();
+    }
+
+private:
+    String* m_protocol;
+    String* m_host;
+    unsigned m_port;
+};
+
+struct SecurityOriginDataHash {
+    size_t operator()(const SecurityOriginData* data) const
+    {
+        return 0;
+    }
+};
+
+struct SecurityOriginDataEqual {
+    bool operator()(const SecurityOriginData* data1,
+                    const SecurityOriginData* data2) const
+    {
+        return *data1 == *data2;
+    }
+};
+}
+
+#endif
---
+++ src/core/storage/Storage.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2017-present Samsung Electronics Co., Ltd
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#ifndef __StarFishStorage__
+#define __StarFishStorage__
+
+#include "binding/ScriptWrappable.h"
+#include "binding/StarFishHoldable.h"
+
+namespace StarFish {
+
+class Storage : public ScriptWrappable, public StarFishHoldable {
+public:
+    virtual void init(ScriptBindingInstance* instance) override;
+    virtual bool isStorage() const override;
+
+    // 4.1 Storage interface in IDL
+    virtual unsigned long length() = 0;
+    virtual String* key(unsigned long index) = 0;
+    virtual String* getItem(String* key) = 0;
+    virtual void setItem(String* key, String* value) = 0;
+    virtual void removeItem(String* key) = 0;
+    virtual void clear() = 0;
+
+protected:
+    Storage(StarFish* starfish)
+        : ScriptWrappable(this)
+        , StarFishHoldable(starfish)
+    {
+    }
+};
+}
+
+#endif
