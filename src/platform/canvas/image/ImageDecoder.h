76c9f5ebfeb3162a544e38a4d54f1d6dc5b690a1
gloryj.kim <gloryj.kim@samsung.com>
2017-06-09 10:42:34 +0900

    Fix the year in license comments
    Signed-off-by: gloryj.kim <gloryj.kim@samsung.com>

--- src/platform/canvas/image/ImageDecoder.h
+++ src/platform/canvas/image/ImageDecoder.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-present Samsung Electronics Co., Ltd
+ * Copyright (c) 2017 Samsung Electronics Co., Ltd
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
+ 
 #ifndef __StarFishImageDecoder__
 #define __StarFishImageDecoder__
 
