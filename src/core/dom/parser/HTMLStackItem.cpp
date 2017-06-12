34825dceda0823471e1e43730a9cec03763c751c
Youngil Choi <duddlf.choi@samsung.com>
2017-06-12 14:30:32 +0900

    Update license clauses
    Signed-off-by: Youngil Choi <duddlf.choi@samsung.com>

--- src/core/dom/parser/HTMLStackItem.cpp
+++ src/core/dom/parser/HTMLStackItem.cpp
@@ -23,7 +23,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 /*
- * Copyright (c) 2017 Samsung Electronics Co., Ltd
+ * Copyright (c) 2017-present Samsung Electronics Co., Ltd
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
