19baecfc42ed548d1c3cfdc60bc23ea9349a691b
김승현/Web Platform Lab(S/W센터)/Engineer/삼성전자 <sh8281.kim@samsung.com>
2017-08-09 18:42:09 +0900

    Use precise GC for FrameTree (#358)
    Signed-off-by: seonghyun kim <sh8281.kim@samsung.com>

--- src/core/layout/FrameText.h
+++ src/core/layout/FrameText.h
@@ -70,6 +70,35 @@ public:
         printf("text-> %s", str.data());
     }
 #endif
+
+    void* operator new(size_t size)
+    {
+        static bool typeInited = false;
+        static GC_descr descr;
+        if (!typeInited) {
+            GC_word obj_bitmap[GC_BITMAP_SIZE(FrameText)] = { 0 };
+            GC_set_bit(obj_bitmap, GC_WORD_OFFSET(FrameText, m_node));
+            GC_set_bit(obj_bitmap,
+                       GC_WORD_OFFSET(FrameText, m_treeItemModel.m_firstChild));
+            GC_set_bit(obj_bitmap,
+                       GC_WORD_OFFSET(FrameText, m_treeItemModel.m_lastChild));
+            GC_set_bit(obj_bitmap,
+                       GC_WORD_OFFSET(FrameText, m_treeItemModel.m_parent));
+            GC_set_bit(obj_bitmap,
+                       GC_WORD_OFFSET(FrameText, m_treeItemModel.m_previous));
+            GC_set_bit(obj_bitmap,
+                       GC_WORD_OFFSET(FrameText, m_treeItemModel.m_next));
+            GC_set_bit(obj_bitmap,
+                       GC_WORD_OFFSET(FrameText, m_treeItemModel.m_firstChild));
+            GC_set_bit(obj_bitmap,
+                       GC_WORD_OFFSET(FrameText, m_treeItemModel.m_lastChild));
+            descr = GC_make_descriptor(obj_bitmap, GC_WORD_LEN(FrameText));
+            typeInited = true;
+        }
+        return GC_MALLOC_EXPLICITLY_TYPED(size, descr);
+    }
+    void* operator new[](size_t size) = delete;
+
 protected:
     virtual bool hasFrameTreeItemModel()
     {
