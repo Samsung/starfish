5b0865e5d92b95def09a68557b3d92e9133867e0
김승현/Web Platform Lab(S/W센터)/Engineer/삼성전자 <sh8281.kim@samsung.com>
2017-08-10 16:33:33 +0900

    Update escargot version (#365)
    Signed-off-by: seonghyun kim <sh8281.kim@samsung.com>

--- inspector/setup_nwjs.sh
+++ inspector/setup_nwjs.sh
@@ -2,6 +2,7 @@
 sudo apt-get install libzmq-dev npm
 wget https://dl.nwjs.io/v0.17.0/nwjs-v0.17.0-linux-x64.tar.gz
 tar xvzf nwjs-v0.17.0-linux-x64.tar.gz
+npm install node-gyp
 npm install zmq
 npm install nw-gyp
 cd node_modules/zmq
