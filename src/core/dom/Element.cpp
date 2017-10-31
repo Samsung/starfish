90885f117f75a3c354341120664da79f66ff034f
변무홍/Web Platform Lab(S/W센터)/Senior Engineer/삼성전자 <mh.byun@samsung.com>
2017-10-31 12:02:11 +0900

    Add guard code at getComputedStyle function. (#801)
    Signed-off-by: MuHong Byun <mh.byun@samsung.com>

--- src/core/dom/Element.cpp
+++ src/core/dom/Element.cpp
@@ -1367,23 +1367,27 @@ CSSStyleDeclaration* Element::getComputedStyle()
     }
 
 // length properties
-#define ADD_ABSOLUTE_LENGTH_PAIR(keyKind, getter)                         \
-    {                                                                     \
-        CSSStyleValuePair p;                                              \
-        p.setKeyKind(CSSStyleValuePair::KeyKind::keyKind);                \
-        if (frame() && frame()->isFrameBox()) {                           \
-            p.setValueKind(CSSStyleValuePair::ValueKind::Length);         \
-            p.setValue(CSSLength(frame()->asFrameBox()->getter()));       \
-        } else if (frame() && frame()->isFrameInline()) {                 \
-            p.setValueKind(CSSStyleValuePair::ValueKind::Length);         \
-            p.setValue(CSSLength(                                         \
-                blockContainer(frame())                                   \
-                    ->firstInlineNonReplacedBox(frame()->asFrameInline()) \
-                    ->getter()));                                         \
-        } else {                                                          \
-            p.setValueKind(CSSStyleValuePair::ValueKind::Auto);           \
-        }                                                                 \
-        d->addValuePair(p);                                               \
+#define ADD_ABSOLUTE_LENGTH_PAIR(keyKind, getter)                     \
+    {                                                                 \
+        CSSStyleValuePair p;                                          \
+        p.setKeyKind(CSSStyleValuePair::KeyKind::keyKind);            \
+        if (frame() && frame()->isFrameBox()) {                       \
+            p.setValueKind(CSSStyleValuePair::ValueKind::Length);     \
+            p.setValue(CSSLength(frame()->asFrameBox()->getter()));   \
+        } else if (frame() && frame()->isFrameInline()) {             \
+            InlineNonReplacedBox* inb =                               \
+                blockContainer(frame())->firstInlineNonReplacedBox(   \
+                    frame()->asFrameInline());                        \
+            if (inb != nullptr) {                                     \
+                p.setValue(CSSLength(inb->getter()));                 \
+                p.setValueKind(CSSStyleValuePair::ValueKind::Length); \
+            } else {                                                  \
+                p.setValueKind(CSSStyleValuePair::ValueKind::Auto);   \
+            }                                                         \
+        } else {                                                      \
+            p.setValueKind(CSSStyleValuePair::ValueKind::Auto);       \
+        }                                                             \
+        d->addValuePair(p);                                           \
     }
     ADD_ABSOLUTE_LENGTH_PAIR(MarginTop, marginTop)
     ADD_ABSOLUTE_LENGTH_PAIR(MarginRight, marginRight)
