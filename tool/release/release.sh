503e4ee1c45b8969252c450c8060653b12bef4a0
Ryan Hyun Choi <ryan.choi@samsung.com>
2018-04-18 10:15:06 +0900

    Add a release to VD script
    Signed-off-by: Ryan Choi <ryan.choi@samsung.com>

--- tool/release/release.sh
+++ tool/release/release.sh
@@ -5,8 +5,6 @@ repo=$1
 
 if [ "$repo" == "lwe_rel" ]; then
     echo "Syncing with: lwe_rel"
-elif [ "$repo" == "lwe_vd" ]; then
-    echo "Syncing with: lwe_vd"
 elif [ "$repo" == "lightweight-web-engine" ]; then
     echo "Syncing with: lightweight-web-engine"
 else
@@ -20,16 +18,13 @@ if [ "$repo" == "lwe_rel" ] && [ ! -d ../lwe_rel ]; then
     # git clone git@github.sec.samsung.net:RS7-webtf/lwe_rel.git ../lwe_rel
     echo "Error: $repo not exist"
     exit 0
-elif [ "$repo" == "lwe_vd" ] && [ ! -d ../lwe_vd ]; then
-    echo "Error: $repo not exist"
-    exit 0
 elif [ "$repo" == "lightweight-web-engine" ] && [ ! -d ../lightweight-web-engine ]; then
     # git clone ssh://id@review.tizen.org:29418/platform/upstream/lightweight-web-engine
     echo "Error: $repo not exist"
     exit 0
-else
-    echo "Found: $repo"
 fi
+
+echo "Found: $repo"
 
 # Syncing with the current starfish repo
 
@@ -44,12 +39,7 @@ make install_header_to_include
 cd $ROOT
 
 python binding_generator/scripts/starfish_code_generator.py src/ src/binding/
-
-if [ "$repo" == "lwe_vd" ]; then
-    rsync -av --delete --include-from "tool/release/release_include.txt" --exclude-from "tool/release/release_exclude.txt" . ../$repo/third_party/lwe
-else
-    rsync -av --delete --include-from "tool/release/release_include.txt" --exclude-from "tool/release/release_exclude.txt" . ../$repo
-fi
+rsync -av --delete --delete-excluded --filter="merge tool/release/filter.txt" --filter="merge tool/release/filter_starfish.txt" . ../$repo
 
 hash=`git log | head -1 | cut -f2 -d' ' | cut -c 1-6`
 today=`date +%y%m%d`
