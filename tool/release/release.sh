188e84e83e4f985991bbc84e76698bac5ed86035
Youngil Choi <duddlf.choi@samsung.com>
2018-05-08 18:15:50 +0900

    Update release script for git hash handling
    Signed-off-by: Youngil Choi <duddlf.choi@samsung.com>

--- tool/release/release.sh
+++ tool/release/release.sh
@@ -41,7 +41,7 @@ cd $ROOT
 python binding_generator/scripts/starfish_code_generator.py src/ src/binding/
 rsync -av --delete --delete-excluded --filter="merge tool/release/filter.txt" --filter="merge tool/release/filter_starfish.txt" . ../$repo
 
-hash=`git log | head -1 | cut -f2 -d' ' | cut -c 1-6`
+hash=`git log | head -1 | cut -f2 -d' ' | cut -c 1-7`
 today=`date +%y%m%d`
 
 cd ../$repo
