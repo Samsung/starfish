c2236ad5c1e3e8990739d9ef415897a4cedf8973
Boram Bae <boram21.bae@samsung.com>
2018-06-14 18:28:06 +0900

    Add dump for network-request
    Signed-off-by: Boram Bae <boram21.bae@samsung.com>

--- src/platform/network/http/HTTPTransaction.cpp
+++ src/platform/network/http/HTTPTransaction.cpp
@@ -170,6 +170,42 @@ void HTTPTransaction::start()
 
     char* LastEffectiveURL = nullptr;
     curl_easy_getinfo(m_curl, CURLINFO_EFFECTIVE_URL, &LastEffectiveURL);
+
+#ifdef STARFISH_ENABLE_TEST
+    if (m_res == CURLE_OK && enableLog) {
+        STARFISH_LOG_INFO("==============Dump Request info==============\n");
+        STARFISH_LOG_INFO("[Request url : %s]\n", m_httpRequest->url().data());
+        double val = 0.0;
+        CURLcode res;
+        res = curl_easy_getinfo(m_curl, CURLINFO_SIZE_DOWNLOAD, &val);
+        if ((CURLE_OK == res) && (val > 0)) {
+            STARFISH_LOG_INFO("[Data downloaded: %.0fbytes]\n", val);
+        }
+
+        res = curl_easy_getinfo(m_curl, CURLINFO_TOTAL_TIME, &val);
+        if ((CURLE_OK == res) && (val > 0)) {
+            STARFISH_LOG_INFO("[Total download time: %.5f sec]\n", val);
+        }
+
+        res = curl_easy_getinfo(m_curl, CURLINFO_SPEED_DOWNLOAD, &val);
+        if ((CURLE_OK == res) && (val > 0)) {
+            STARFISH_LOG_INFO("[Average download speed: %.0f kbyte/sec]\n",
+                              val / 1024);
+        }
+
+        res = curl_easy_getinfo(m_curl, CURLINFO_NAMELOOKUP_TIME, &val);
+        if ((CURLE_OK == res) && (val > 0)) {
+            STARFISH_LOG_INFO("[Name lookup time: %.5f sec]\n", val);
+        }
+
+        res = curl_easy_getinfo(m_curl, CURLINFO_CONNECT_TIME, &val);
+        if ((CURLE_OK == res) && (val > 0)) {
+            STARFISH_LOG_INFO("[Connect time: %.5f sec]\n", val);
+        }
+        STARFISH_LOG_INFO("=============================================\n");
+    }
+#endif
+
     if (LastEffectiveURL) {
         m_httpResponse->setLastEffectiveURL(LastEffectiveURL);
         // Do not free LastEffectiveURL
