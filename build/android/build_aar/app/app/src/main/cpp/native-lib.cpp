161fe44543c58a24c2fef78d0dd8056a30ee4385
MuHong Byun <mh.byun@samsung.com>
2018-07-19 13:26:22 +0900

    Add build project file for android
    Signed-off-by: MuHong Byun <mh.byun@samsung.com>

---
+++ build/android/build_aar/app/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,13 @@
+#include <jni.h>
+#include <string>
+
+extern "C"
+JNIEXPORT jstring
+
+JNICALL
+Java_android_sec_com_lightweightwebengine_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
