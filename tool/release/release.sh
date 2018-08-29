d872e5ff3c0b96a55ca5d6fed31fa6b00c08b316
Youngil Choi <duddlf.choi@samsung.com>
2018-08-29 19:01:06 +0900

    Update release script
    Signed-off-by: Youngil Choi <duddlf.choi@samsung.com>

--- tool/release/release.sh
+++ tool/release/release.sh
@@ -29,7 +29,7 @@ echo "Found: $repo"
 # Syncing with the current starfish repo
 
 git submodule init
-git submodule update binding_generator third_party/deviceapi third_party/escargot tool/gyp
+git submodule update
 
 cd third_party/escargot
 git submodule init
