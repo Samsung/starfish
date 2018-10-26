79fb7ebf61b2ec4a712521f641f0cf1235c82aa6
김현준/Tizen Platform Lab(SR)/Engineer/삼성전자 <hyunjune.kim@samsung.com>
2018-10-26 17:11:40 +0900

    Fix up the overflow issue with flex (#2143)
    Signed-off-by: Hyunjune Kim <hyunjune.kim@samsung.com>

--- src/core/layout/FrameBox.cpp
+++ src/core/layout/FrameBox.cpp
@@ -3326,15 +3326,15 @@ LayoutUnit FrameBox::minMaxWidthAppliedIfNeeds(
                                     parentWidth - mbpWidth());
             p.computePreferredWidth();
             minWidth = std::min(minWidth, p.preferredMinWidth());
-        }
-
-        if (isFlexItem() && style->width().isPercent()) {
+        } else if (style->width().isPercent() &&
+                   appliedOverflowX() == VisibleOverflow) {
             PreferredWidthContext p(ctx, nullptr, this, this,
                                     parentWidth - mbpWidth());
             Length oldWidth = style->width();
             style->setWidth(Length());
             p.computePreferredWidth();
             style->setWidth(oldWidth);
+
             minWidth = std::min(minWidth, p.preferredWidth());
             minWidth = std::min(minWidth, p.preferredMinWidth());
         }
