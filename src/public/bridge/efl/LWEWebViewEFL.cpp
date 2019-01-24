576557d2548f3c27cb52feed8f78eef816a5f366
최영일/Common Platform Lab(SR)/Principal Engineer/삼성전자 <duddlf.choi@samsung.com>
2019-01-24 17:13:38 +0900

    Resolving Static Anaylsis Defect (#2383)
    Signed-off-by: Youngil Choi <duddlf.choi@samsung.com>

--- src/public/bridge/efl/LWEWebViewEFL.cpp
+++ src/public/bridge/efl/LWEWebViewEFL.cpp
@@ -247,6 +247,10 @@ public:
                const char* timezoneID)
         : WebView(nullptr)
         , m_lastInputTime(0)
+        , m_lastMouseX(0)
+        , m_lastMouseY(0)
+        , m_isMouseLbuttonDown(false)
+        , m_isKeyDown(false)
     {
         STARFISH_LOG_INFO("WebViewEFL::WebViewEFL");
         Evas_Object* win = (Evas_Object*)winArg;
