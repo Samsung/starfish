a94e6136f3d8a74239d029b67dcc797519ee916e
최현/Common Platform Lab(SR)/Staff Engineer/삼성전자 <ryan.choi@samsung.com>
2019-04-15 16:34:03 +0900

    Update android build script (#2581)
    Signed-off-by: Ryan Choi <ryan.choi@samsung.com>

--- tool/release/release-android.sh
+++ tool/release/release-android.sh
@@ -30,6 +30,7 @@ python binding_generator/scripts/starfish_code_generator.py src/ src/binding/
 
 rsync -av --delete --delete-excluded --filter="merge tool/release/filter-android.txt" build/android/apk_root/ ../$repo
 rsync -av --delete --delete-excluded --filter="merge tool/release/filter-android.txt" . ../$repo/src/starfish
+cp -f .gitignore ../$repo
 
 hash=`git log | head -1 | cut -f2 -d' ' | cut -c 1-7`
 today=`date +%y%m%d`
