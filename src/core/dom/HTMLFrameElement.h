6f02499ef8844bb1277e7ee6ba3026e9856adde2
변무홍/MDE Lab(SR)/삼성전자 <mh.byun@samsung.com>
2023-04-13 12:39:54 +0900

    Add HTMLFrameElement,HTMLFrameSetElement (#3729)
    * Skeleton code only, Added to avoid errors in certain JS implementations.
    * Apply review comment
    
    Signed-off-by: Byun MuHong <mh.byun@samsung.com>

---
+++ src/core/dom/HTMLFrameElement.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2023-present Samsung Electronics Co., Ltd
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ *  USA
+ */
+
+#ifndef __StarfishHTMLFrameElement__
+#define __StarfishHTMLFrameElement__
+
+#include "core/dom/HTMLElement.h"
+
+namespace Starfish {
+
+class HTMLFrameElement : public HTMLElement {
+public:
+    HTMLFrameElement(Document* document, const QualifiedName& qname)
+        : HTMLElement(document, qname)
+    {
+    }
+
+    virtual void init(ScriptBindingInstance* instance,
+                      void* domObjectPointer) override;
+    virtual bool isHTMLFrameElement() const override;
+};
+} // namespace Starfish
+
+#endif
