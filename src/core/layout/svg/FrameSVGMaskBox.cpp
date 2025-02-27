c67e0eb35f4e3c280571cc709b9ec65c323871d6
Seonghyun Kim <sh8281.kim@samsung.com>
2025-02-27 20:01:02 +0900

    Fixup SVG Luminance mask
    * Improve performance by removing double conversion and multiply
    * Fix bug(We should apply conversion every pixel. not only a == 255)
    
    Signed-off-by: Seonghyun Kim <sh8281.kim@samsung.com>

--- src/core/layout/svg/FrameSVGMaskBox.cpp
+++ src/core/layout/svg/FrameSVGMaskBox.cpp
@@ -41,15 +41,17 @@ static void makeLuminanceMask(uint8_t* ptr, size_t w, size_t s, size_t h)
         uint8_t* p = ptr;
         for (size_t x = 0; x < w; x++) {
             uint8_t a = p[3];
-            if (a == 255) {
-                uint8_t r = p[0];
-                uint8_t g = p[1];
-                uint8_t b = p[2];
-
-                double luma = (r * 0.2125 + g * 0.7154 + b * 0.0721) *
-                              ((double)a / 255.0);
-                p[3] = luma;
-            }
+#ifdef PORT_PIXEL_ORDER_RGBA
+            uint32_t r = p[0];
+            uint32_t g = p[1];
+            uint32_t b = p[2];
+#else
+            uint32_t r = p[2];
+            uint32_t g = p[1];
+            uint32_t b = p[0];
+#endif
+            *reinterpret_cast<uint32_t*>(p) =
+                ((r * 109 + g * 366 + b * 37 + 256) << 15) & 0xff000000;
             p += 4;
         }
         ptr += s;
