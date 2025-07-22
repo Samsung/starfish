65bf5d1e85d6ac7e14b4cff4a1c5a89170d01712
MuHong Byun <mh.byun@samsung.com>
2025-07-22 15:17:04 +0900

    Improve CSS custom property handling in tokenization
    Signed-off-by: MuHong Byun <mh.byun@samsung.com>

--- src/core/style/CSSStyleDeclaration.cpp
+++ src/core/style/CSSStyleDeclaration.cpp
@@ -44,6 +44,31 @@
 
 namespace Starfish {
 
+//  https://github.com/lattera/freebsd/blob/master/lib/libc/string/strnstr.c#L42
+/*
+ * Find the first occurrence of find in s, where the search is limited to the
+ * first slen characters of s.
+ */
+char* strnstr(const char* s, const char* find, size_t slen)
+{
+    char c, sc;
+    size_t len;
+
+    if ((c = *find++) != '\0') {
+        len = strlen(find);
+        do {
+            do {
+                if (slen-- < 1 || (sc = *s++) == '\0')
+                    return (NULL);
+            } while (sc != c);
+            if (len > slen)
+                return (NULL);
+        } while (strncmp(s, find, len) != 0);
+        s--;
+    }
+    return ((char*)s);
+}
+
 static bool UnitPositionShorthandToLongHand(
     const CSSStyleValuePair::KeyKind& shorthand,
     CSSStyleValuePair::KeyKind& longhandX,
@@ -1974,7 +1999,8 @@ bool CSSStyleDeclaration::setPropertyInternalFor<
     } else if (UNLIKELY(keyKind == CSSStyleValuePair::KeyKind::Transform)) {
         tokenizeCSSValue(tokens, value, valueLength, ",", 1, true);
     } else {
-        tokenizeCSSValue(tokens, value, valueLength, ",", 1);
+        bool hasCustomProperty = strnstr(value, "--", valueLength);
+        tokenizeCSSValue(tokens, value, valueLength, ",", 1, hasCustomProperty);
     }
 
     bool needToRemoveAndUpdate = shouldKeepAppearanceOrder(keyKind);
