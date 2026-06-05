4abe71c4927da1e6a6b63e93a6da7544fb6ff8de
Seonghyun Kim <sh8281.kim@samsung.com>
2026-06-05 15:23:34 +0900

    Don't specify depth, stencil, MSAA while config gl in evasGL
    Signed-off-by: Seonghyun Kim <sh8281.kim@samsung.com>

--- src/public/bridge/efl/LWEWebViewEFL.cpp
+++ src/public/bridge/efl/LWEWebViewEFL.cpp
@@ -347,9 +347,6 @@ public:
         // Set a surface config
         m_glCfg = evas_gl_config_new();
         m_glCfg->color_format = EVAS_GL_RGBA_8888;
-        m_glCfg->depth_bits = EVAS_GL_DEPTH_NONE;
-        m_glCfg->stencil_bits = EVAS_GL_STENCIL_NONE;
-        m_glCfg->multisample_bits = EVAS_GL_MULTISAMPLE_NONE;
 
 // we need to set these secret flags reducing memory usage
 // see platform/upstream/efl/src/modules/evas/engines/gl_common/evas_gl_core.c
